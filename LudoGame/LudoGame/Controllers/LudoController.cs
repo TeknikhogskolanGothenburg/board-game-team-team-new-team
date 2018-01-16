270401daf28136a85fcfd3b77b310eae107d342a
Kalle Blomnell <Karlmikaelb@gmail.com>
2018-01-17 00:45:35 +0100

    Added a View and a Controller

---
+++ LudoGame/LudoGame/Controllers/LudoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LudoGame.Controllers
+{
+    public class LudoController : Controller
+    {
+        // GET: /Ludo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /Ludo/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
