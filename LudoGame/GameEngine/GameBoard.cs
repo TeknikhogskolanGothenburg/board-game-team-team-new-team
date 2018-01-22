1e8f0979c2026341fb2e4c940cafef34c01770de
Slotheron <32925757+Slotheron@users.noreply.github.com>
2018-01-22 08:30:02 +0100

    added board among other classes to use in the future

---
+++ LudoGame/GameEngine/GameBoard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    //Class will hold all peice logic together and run in the head controller.
+    public class GameBoard
+    {
+        public List<GamePiece> GamePieces;
+        public List<GameSquare> GameSquares;
+
+
+        public void PlacePiece(GamePiece piece, GameSquare square)
+        {
+            //do stuff
+            //to initialize first square depending on piece color.
+        }
+        public void MovePiece(GameSquare originSquare, GameSquare targetSquare)
+        {
+            //do stuff
+            //to move a game piece from one square to another
+        }
+        public void RemovePiece(GameSquare square)
+        {
+            //do stuff
+            //to remove a game piece or peices from a specified square
+        }
+    }
+}
---
+++ LudoGame/GameEngine/GameSquare.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class GameSquare
+    {
+        //a numbered name using just an integer till the last square, t.ex
+        // 1, 2, 3, 1Y, 2Y
+        public string NumberedName;
+    }
+}
