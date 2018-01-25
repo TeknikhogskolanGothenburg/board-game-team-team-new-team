452b2b27502c89b23dfc1f8093f2d816f6c38a6a
Kalle Blomnell <Karlmikaelb@gmail.com>
2018-01-25 09:19:34 +0100

    New graphic!

--- LudoGame/GameEngine/GameBoard.cs
+++ LudoGame/GameEngine/GameBoard.cs
@@ -11,22 +11,5 @@ namespace GameEngine
     {
         public List<GameSquare> GameSquares = new List<GameSquare>();
         public List<GamePlayer> GamePlayers = new List<GamePlayer>();
-
-
-        public void PlacePiece(GamePiece piece, GameSquare square)
-        {
-            //do stuff
-            //to initialize first square depending on piece color.
-        }
-        public void MovePiece(GameSquare originSquare, GameSquare targetSquare)
-        {
-            //do stuff
-            //to move a game piece from one square to another
-        }
-        public void RemovePiece(GameSquare square)
-        {
-            //do stuff
-            //to remove a game piece or peices from a specified square
-        }
     }
 }
